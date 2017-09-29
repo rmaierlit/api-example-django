e409c0cc3f34b109aebe86c63d7835fe862dea54
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-09-29 14:09:05 -0700

    implements full OAuth routing

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -1,11 +1,14 @@
+"""urls and routing for this project"""
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
-import views
+from . import views
 
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+
+    url(r'check-in', views.check_in, name="check_in"),
 
     url(r'', include('social.apps.django_app.urls', namespace='social')),
 ]
--- drchrono/views.py
+++ drchrono/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+"""this modules contains the views for this project
+"""
+
+from django.http import HttpResponse
+
+def check_in(request):
+    """testing to make sure this route works"""
+    return HttpResponse('sorry g, we\'re closed')
